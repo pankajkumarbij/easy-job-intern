e1a95fe9a84ec92fb7c966701da19c6e5c02c965
pankaj kumar bijarniya <59636849+pankajkumarbij@users.noreply.github.com>
2021-02-17 08:21:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import './App.css';
 import NavBar from './components/navbar';
-<<<<<<< HEAD
 import LoginForm from "./components/login";
 import RegisterForm from "./components/register";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-=======
 import Footer from './components/Footer';
 import AboutUs from './components/AboutUs';
->>>>>>> 4a1c9dc4d9436a5d141718e152056e62b193f6d7
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-    
     <Router>
-      
       <NavBar />
       <h1>Welcome to Easy Job Intern</h1>
-    <Switch>
-    
+      <Switch>
           <Route path="/login" exact component={LoginForm} />
           <Route path="/signup" exact component={RegisterForm} />
       </Switch>
-      
-    </Router>
-
-=======
-      <NavBar/>
-      {/* <h1>Welcome to Easy Job Intern</h1> */}
+      </Router>
       <AboutUs />
-      {/* <Footer/> */}
->>>>>>> 4a1c9dc4d9436a5d141718e152056e62b193f6d7
+     <Footer/>
     </div>
 
     
