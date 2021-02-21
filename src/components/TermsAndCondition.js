48acd0bdda08fb4ec773eb1b5bdc5501fbfe3e1b
soumya-badkul <badkulsoumya@gmail.com>
2021-02-21 12:15:12 +0530

    updated the changes

--- src/components/TermsAndCondition.js
+++ src/components/TermsAndCondition.js
@@ -5,28 +5,37 @@ import {Container, Row, Col} from "react-bootstrap";
 export default function TermsAndConditions() {
     return (
         <>
-            <Container style={{width:'100%', background:"#BDEDFF", textAlign:'center'}}>
-            <h2 style={{fontFamily:'revert', color:'maroon'}}>TERMS AND CONDITIONS</h2>
-            <br/>
+            <div style={{background: 'linear-gradient(to bottom, #ccffff 3%, #ccccff 30%)'}}>
+        <h2 style={{fontFamily:'revert', color:'#660066', textAlign:'center', paddingTop:'2%'}}>Privacy Policy</h2>
+            <Container style={{width:'100%', background:"None", textAlign:'left'}}>
                 <Row>
-                    <Col class="col-12 col-md-12 col-sm-12">
-                    <h6 style={{fontFamily:'revert', color:'maroon'}}>LOREM IPSUM</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <Col class="col-6">
+                        <h5 style={{color:'#660066'}}>LOREM IPSUM</h5> 
+                        <ul style={{listStyle: 'none'}}>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                        </ul>                 
                     </Col>
+                    <Col class="col-6">
+                        <h5 style={{color:'#660066'}}>LOREM IPSUM</h5> 
+                        <ul style={{listStyle: 'none'}}>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                        </ul>                 
+                    </Col>
+                    
                     </Row>
-                    <Row>
-                    <Col class="col-6">
-                    <h6 style={{fontFamily:'revert', color:'maroon'}}>LOREM IPSUM</h6>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </Col>
-                    <Col class="col-6">
-                    <h6 style={{fontFamily:'revert', color:'maroon'}}>LOREM IPSUM</h6>
+                    <Row>                
+                    <Col class="col-12 col-md-12 col-sm-12">
+                        <h5 style={{color:'#660066'}}>LOREM IPSUM</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     </Col>
                 </Row>
             </Container>
             
+         </div>
         </>
     )
 }
