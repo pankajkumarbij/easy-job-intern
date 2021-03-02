eb8f85c94a92f14d7b09b6927064601e92182f8f
sukhpreet-singh1 <493_bt19@iiitkalyani.ac.in>
2021-03-02 20:00:51 +0530

    made contact us responsive and improved design

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -6,31 +6,25 @@ const contactUs = () => {
         <div className="background" >
 
                 <div className="form">
+                      <h3>Contact Us</h3>
+                    <Form className="contactUs">
 
-                    <Form>
-                        <h3>Contact Us</h3>
-                        <Row>
-                            <Col md={6} xs={12}>
-                                <Form.Label>First Name</Form.Label>
-                                <Form.Control placeholder="First name" />
-                            </Col>
-                            <Col md={6} xs={12}>
-                                <Form.Label>Last Name</Form.Label>
-                                <Form.Control placeholder="Last name" />
-                            </Col>
-                        </Row>
+                        <Form.Group>
+                                    <Form.Control placeholder="First name" />
+                        </Form.Group>
+                        <Form.Group>
+                                    <Form.Control placeholder="Last name" />
+                        </Form.Group>
+
                         <Form.Group controlId="formBasicEmail">
-                            <Form.Label className="form-name">Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Contact No.</Form.Label>
                             <Form.Control type="text" placeholder="Contact No." />
                         </Form.Group>
                         <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Message</Form.Label>
-                            <Form.Control as="textarea" rows={3} />
+                            <Form.Control placeholder="Message..." as="textarea" rows={3} />
                         </Form.Group>
                         <Form.Group controlId="formBasicCheckbox">
                             <Form.Check type="checkbox" label="Check me out"/>
