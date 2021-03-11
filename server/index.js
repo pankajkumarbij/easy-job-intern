0dc4a5ef42f090242465c736170db711ae0a6b12
Kartik Puri <52126579+kartikpuri99@users.noreply.github.com>
2021-03-11 16:09:03 +0530

    connected contact us form to database

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
-const express=require("express");
-const PORT=5000;
+const express = require("express");
+const PORT = 5000;
 
 const app = express();
 
-const ContactRouter = require('./routes/contact-us');
+const ContactRouter = require("./routes/contact-us");
 
-app.use('/contact',ContactRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/contact", ContactRouter);
 
-app.get('/',(req,res)=>{
-    res.send("Welcome to easy job intern server");
+app.get("/", (req, res) => {
+  res.send("Welcome to easy job intern server");
 });
 
-app.listen(PORT,()=>{
-    console.log("server is running on port ",PORT)
-})
+app.listen(PORT, () => {
+  console.log("server is running on port ", PORT);
+});
--- server/routes/contact-us.js
+++ server/routes/contact-us.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/"
 
 const ContactSchema = require("../models/contact-us-model");
 
 router.post("/contact-us", (req, res) => {
   var { email, first_name, last_name, message, contact } = req.body;
-  email = email.trim();
-  first_name = first_name.trim();
-  last_name = last_name.trim();
-  contact = contact.trim();
-  MongoClient.connect(url, function(err, db) {
+  // console.log(email,first_name,last_name,message,contact);
+  email = email ? email.trim() : "";
+  first_name = first_name ? first_name.trim() : "";
+  last_name = last_name ? last_name.trim() : "";
+  contact = contact ? contact.trim() : "";
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("eji");
-    const newMessage = new ContactSchema({first_name,last_name,email,contact,message});
-    dbo.collection("ContactMessage").insertOne(newMessage, function(err, result) {
-      if (err) throw err;
-      console.log(result.ops[0]);
-      res.send(result.ops[0]);
-      db.close();
+    const newMessage = new ContactSchema({
+      first_name,
+      last_name,
+      email,
+      contact,
+      message,
     });
+    dbo
+      .collection("ContactMessage")
+      .insertOne(newMessage, function (err, result) {
+        if (err) throw err;
+        console.log(result.ops[0]);
+        res.send(result.ops[0]);
+        db.close();
+      });
   });
-})
+});
 
 module.exports = router;
