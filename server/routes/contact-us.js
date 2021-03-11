ec3a67eb7ce441c6bf56c56bb044af08e15a646c
hrushikesh-git <hrushikeshrasne@gmail.com>
2021-03-11 14:59:45 +0530

    Added Login and Signup modals and routes and Integrated with react frontend

--- server/routes/contact-us.js
+++ server/routes/contact-us.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const router = express.Router();
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/"
+const mongoose = require('mongoose')
 
 const ContactSchema = require("../models/contact-us-model");
 
 router.post("/contact-us", (req, res) => {
   var { email, first_name, last_name, message, contact } = req.body;
-  email = email.trim();
-  first_name = first_name.trim();
-  last_name = last_name.trim();
-  contact = contact.trim();
-  MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("eji");
-    const newMessage = new ContactSchema({first_name,last_name,email,contact,message});
-    dbo.collection("ContactMessage").insertOne(newMessage, function(err, result) {
-      if (err) throw err;
-      console.log(result.ops[0]);
-      res.send(result.ops[0]);
-      db.close();
-    });
-  });
+ if(!email || !first_name || !last_name || !message){
+   return res.status(422).json({
+     error:"Please fill all fields"
+   })
+ }
+  const contactMessage = new ContactSchema({
+    email,
+        first_name,
+        last_name,
+        message,
+        contact
+  })
+  contactMessage.save()
+  .then(data=>{
+    res.json(data)
+  })
+  .catch(error=>{console.log(error)})
 })
 
 module.exports = router;
