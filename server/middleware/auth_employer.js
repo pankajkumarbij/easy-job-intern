0634a647286513ade3fcb78b0907f2d5579e9e57
AbhishekTiwari07 <abhishekt2024@gmail.com>
2021-03-22 11:06:22 +0530

    Added middleware and token in models

--- server/middleware/auth_employer.js
+++ server/middleware/auth_employer.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken')
-const User = require('../models/employer.js')
+const Employer = require('../models/employer.js')
 const {JWT_SECRET} = require('../keys')
 
 const auth_employer = async (req,res,next) =>{
     try{
+        // Authentication type Bearer
         const token = req.header('Authorization').replace('Bearer ','')
         const decoded = jwt.verify(token,JWT_SECRET)
-        const user = await User.findOne({ _id : decoded._id, 'tokens.token':token})
+        const user = await Employer.findOne({ _id : decoded._id, 'tokens.token':token})
         if(!user){
             throw new Error("error")
         }
--- server/middleware/auth_student.js
+++ server/middleware/auth_student.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken')
-const User = require('../models/student.js')
+const Student = require('../models/student.js')
 const {JWT_SECRET} = require('../keys')
 
 const auth_student = async (req,res,next) =>{
     try{
+        // Authentication type Bearer
         const token = req.header('Authorization').replace('Bearer ','')
         const decoded = jwt.verify(token,JWT_SECRET)
-        const user = await User.findOne({ _id : decoded._id, 'tokens.token':token})
+        const user = await Student.findOne({ _id : decoded._id, 'tokens.token':token})
         if(!user){
             throw new Error("error")
         }
