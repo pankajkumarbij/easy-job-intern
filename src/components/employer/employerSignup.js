d6be5de039517428713cf6b62aa764f7da85eaa2
Kartik Puri <52126579+kartikpuri99@users.noreply.github.com>
2021-03-23 16:39:33 +0530

    made employer login and signup form responsive

--- src/components/employer/employerSignup.js
+++ src/components/employer/employerSignup.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Form from "react-bootstrap/Form";
+import { Button, Card, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import axios from 'axios';
-
-
+import axios from "axios";
+import './employer.css'
 
 class EmployerSignup extends Component {
   constructor() {
@@ -17,7 +14,7 @@ class EmployerSignup extends Component {
       contact: null,
       password: null,
       passwordConfirmation: null,
-      signupError: null
+      signupError: null,
     };
   }
 
@@ -42,8 +39,8 @@ class EmployerSignup extends Component {
         this.setState({ personName: e.target.value });
         break;
       case "contact":
-          this.setState({ contact: e.target.value });
-          break;
+        this.setState({ contact: e.target.value });
+        break;
       default:
         break;
     }
@@ -58,167 +55,165 @@ class EmployerSignup extends Component {
     if (!this.formIsValid()) {
       this.setState({ signupError: "Passwords do not match" });
       return;
+    } else {
+      axios
+        .post("http://localhost:5000/employer/signup", {
+          personName: this.state.personName,
+          email: this.state.email,
+          password: this.state.password,
+          contact: this.state.contact,
+          passwordConfirmation: this.state.passwordConfirmation,
+          companyName: this.state.companyName,
+        })
+        .then((res) => {
+          console.log(res.data.user);
+          alert(res.data.message);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-    else{
-      axios.post('http://localhost:5000/employer/signup',{
-        personName: this.state.personName,
-        email: this.state.email,
-        password:this.state.password,
-        contact: this.state.contact,
-        passwordConfirmation: this.state.passwordConfirmation,
-        companyName: this.state.companyName
-    })
-    .then((res) => {
-      console.log(res.data.user);
-     alert(res.data.message);
-    
-    }).catch((err) => {
-      console.log(err);
-    })
-    }
-  
   };
 
   render() {
     return (
       <>
-      <div style={{padding:"4vh 0"}}>
-        <Card
-          style={{
-            width: "40vw",
-            marginLeft: "auto",
-            marginRight: "auto",
-            marginTop: "4vh",
-            marginBottom: "4vh",
-            backgroundImage: "linear-gradient(to right, white , #6EE2CD)"
-          }}
-        >
-          <Card.Header
+        <div style={{ padding: "4vh 0" }}>
+          <Card
             style={{
-              backgroundColor: "#6c6c6c",
-              color: "#6EE2CD",
-              fontFamily: '"Merriweather", serif',
-              fontSize: "1.25rem",
+              width: "40vw",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: "4vh",
+              marginBottom: "4vh",
+              backgroundImage: "linear-gradient(to right, white , #6EE2CD)",
             }}
-            as="h5"
+            className="employer_form_card_custom"
           >
-            Employer Signup
-          </Card.Header>
-          <Card.Body>
-            <Form onSubmit={(e) => this.submitSignup(e)}>
-
-            <Form.Group
-                style={{ textAlign: "left" }}
-                controlId="formBasicName"
-                onChange={(e) => this.userTyping("companyName", e)}
-              >
-                <Form.Label style={{ fontWeight: "bold" }}>
-                 Company Name
-                </Form.Label>
-                <Form.Control
-                  style={{ borderColor: "#6EE2CD", color: "#000000" }}
-                  type="text"
-                  placeholder="Enter the company name"
-                />
-              </Form.Group>
-
-
-              <Form.Group
-                style={{ textAlign: "left" }}
-                controlId="formBasicEmail"
-                onChange={(e) => this.userTyping("email", e)}
-              >
-                <Form.Label style={{ fontWeight: "bold" }}>
-                  Email address
-                </Form.Label>
-                <Form.Control
-                  style={{ borderColor: "#6EE2CD", color: "#000000" }}
-                  type="email"
-                  placeholder="Enter email"
-                />
-              </Form.Group>
-
-              <Form.Group
-                style={{ textAlign: "left" }}
-                controlId="formBasicPassword"
-                onChange={(e) => this.userTyping("password", e)}
-              >
-                <Form.Label style={{ fontWeight: "bold" }}>Password</Form.Label>
-                <Form.Control
-                  style={{ borderColor: "#6EE2CD", color: "#000000" }}
-                  type="password"
-                  placeholder="Password"
-                />
-              </Form.Group>
-              <Form.Group
-                style={{ textAlign: "left", marginBottom: "1.6vh" }}
-                controlId="formBasicPassword"
-                onChange={(e) => this.userTyping("passwordConfirmation", e)}
-              >
-                <Form.Label style={{ fontWeight: "bold" }}>
-                  Confirm Password
-                </Form.Label>
-                <Form.Control
-                  style={{ borderColor: "#6EE2CD",color: "#000000" }}
-                  type="password"
-                  placeholder="Re-enter Password"
-                />
-              </Form.Group>
-
-              <Form.Group
-                style={{ textAlign: "left" }}
-                onChange={(e) => this.userTyping("personName", e)}
-              >
-                <Form.Label style={{ fontWeight: "bold" }}>
-                  Name
-                </Form.Label>
-                <Form.Control
-                  style={{ borderColor: "#6EE2CD", color: "#000000" }}
-                  type="text"
-                  placeholder="Enter your name"
-                />
-              </Form.Group>
-
-              <Form.Group
-                style={{ textAlign: "left" }}
-                onChange={(e) => this.userTyping("contact", e)}
-              >
-                <Form.Label style={{ fontWeight: "bold" }}>
-                Contact
-                </Form.Label>
-                <Form.Control
-                  style={{ borderColor: "#6EE2CD", color: "#000000" }}
-                  type="number"
-                  placeholder="Enter your contact number"
-                />
-              </Form.Group>
-
-              <Form.Group style={{ textAlign: "left", fontSize: "1.5vh" }}>
-                <Link to="/employer-login">
-                  <a style={{ fontWeight: "bold" }}>
-                    Already have an account? Sign in
-                  </a>
-                </Link>
-              </Form.Group>
-
-              {this.state.signupError ? (
-                <Form.Text
-                  style={{ paddingBottom: "0.6vh", fontWeight: "bold" }}
-                  className="text-danger"
+            <Card.Header
+              style={{
+                backgroundColor: "#6c6c6c",
+                color: "#6EE2CD",
+                fontFamily: '"Merriweather", serif',
+                fontSize: "1.25rem",
+              }}
+              as="h5"
+            >
+              Employer Signup
+            </Card.Header>
+            <Card.Body>
+              <Form onSubmit={(e) => this.submitSignup(e)}>
+                <Form.Group
+                  style={{ textAlign: "left" }}
+                  controlId="formBasicName"
+                  onChange={(e) => this.userTyping("companyName", e)}
                 >
-                  {this.state.signupError}
-                </Form.Text>
-              ) : null}
-              <Button
-                style={{ color: "#6EE2CD", fontWeight: "bold" }}
-                variant="secondary"
-                type="submit"
-              >
-                Register
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
+                  <Form.Label style={{ fontWeight: "bold" }}>
+                    Company Name
+                  </Form.Label>
+                  <Form.Control
+                    style={{ borderColor: "#6EE2CD", color: "#000000" }}
+                    type="text"
+                    placeholder="Enter the company name"
+                  />
+                </Form.Group>
+
+                <Form.Group
+                  style={{ textAlign: "left" }}
+                  controlId="formBasicEmail"
+                  onChange={(e) => this.userTyping("email", e)}
+                >
+                  <Form.Label style={{ fontWeight: "bold" }}>
+                    Email address
+                  </Form.Label>
+                  <Form.Control
+                    style={{ borderColor: "#6EE2CD", color: "#000000" }}
+                    type="email"
+                    placeholder="Enter email"
+                  />
+                </Form.Group>
+
+                <Form.Group
+                  style={{ textAlign: "left" }}
+                  controlId="formBasicPassword"
+                  onChange={(e) => this.userTyping("password", e)}
+                >
+                  <Form.Label style={{ fontWeight: "bold" }}>
+                    Password
+                  </Form.Label>
+                  <Form.Control
+                    style={{ borderColor: "#6EE2CD", color: "#000000" }}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Group>
+                <Form.Group
+                  style={{ textAlign: "left", marginBottom: "1.6vh" }}
+                  controlId="formBasicPassword"
+                  onChange={(e) => this.userTyping("passwordConfirmation", e)}
+                >
+                  <Form.Label style={{ fontWeight: "bold" }}>
+                    Confirm Password
+                  </Form.Label>
+                  <Form.Control
+                    style={{ borderColor: "#6EE2CD", color: "#000000" }}
+                    type="password"
+                    placeholder="Re-enter Password"
+                  />
+                </Form.Group>
+
+                <Form.Group
+                  style={{ textAlign: "left" }}
+                  onChange={(e) => this.userTyping("personName", e)}
+                >
+                  <Form.Label style={{ fontWeight: "bold" }}>Name</Form.Label>
+                  <Form.Control
+                    style={{ borderColor: "#6EE2CD", color: "#000000" }}
+                    type="text"
+                    placeholder="Enter your name"
+                  />
+                </Form.Group>
+
+                <Form.Group
+                  style={{ textAlign: "left" }}
+                  onChange={(e) => this.userTyping("contact", e)}
+                >
+                  <Form.Label style={{ fontWeight: "bold" }}>
+                    Contact
+                  </Form.Label>
+                  <Form.Control
+                    style={{ borderColor: "#6EE2CD", color: "#000000" }}
+                    type="number"
+                    placeholder="Enter your contact number"
+                  />
+                </Form.Group>
+
+                <Form.Group style={{ textAlign: "left", fontSize: "1.5vh" }}>
+                  <Link to="/employer-login">
+                    <a style={{ fontWeight: "bold" }}>
+                      Already have an account? Sign in
+                    </a>
+                  </Link>
+                </Form.Group>
+
+                {this.state.signupError ? (
+                  <Form.Text
+                    style={{ paddingBottom: "0.6vh", fontWeight: "bold" }}
+                    className="text-danger"
+                  >
+                    {this.state.signupError}
+                  </Form.Text>
+                ) : null}
+                <Button
+                  style={{ color: "#6EE2CD", fontWeight: "bold" }}
+                  variant="secondary"
+                  type="submit"
+                >
+                  Register
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
         </div>
       </>
     );
