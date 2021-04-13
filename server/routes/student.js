f065887b6365869f0abf24125e3fde3fb57db188
mitalirs <mitalirs1701@gmail.com>
2021-04-13 17:23:40 +0530

    added search filter for internships

--- server/routes/student.js
+++ server/routes/student.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { signup, signin, update, logout, logoutAll } = require("../controller/student.auth");
 const auth_student = require('../middleware/auth_student');
+const {searchFilter} = require("../controller/internships")
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.patch("/update", auth_student, update);
 router.get('/logout', auth_student, logout);
 router.get('/logoutAll', auth_student, logoutAll);
+router.get('/searchFilter', auth_student, searchFilter);
 
 module.exports = router
