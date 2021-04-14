63e1b5f32d62dd0e55bc400ad0eae609b233e439
mitalirs <mitalirs1701@gmail.com>
2021-04-14 13:51:12 +0530

    filter for internships

--- server/routes/student.js
+++ server/routes/student.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { signup, signin, update, logout, logoutAll } = require("../controller/student.auth");
 const auth_student = require('../middleware/auth_student');
+const {searchFilterInternships} = require("../controller/internships")
 
 
 router.post("/signup", signup);
@@ -9,6 +10,7 @@ router.post("/signin", signin);
 router.patch("/update", auth_student, update);
 router.get('/logout', auth_student, logout);
 router.get('/logoutAll', auth_student, logoutAll);
+router.get('/searchFilterInternships', auth_student, searchFilterInternships);
 
 
 module.exports = router
