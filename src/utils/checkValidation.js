c9b2f35bd0237083c0ee0b47f0c1754a9c05403a
Himanshu Jaidka <himanshu.jaidka@gmail.com>
2021-04-14 16:07:05 +0530

    Update checkValidation.js

--- src/utils/checkValidation.js
+++ src/utils/checkValidation.js
@@ -19,7 +19,7 @@ const checkValidity = (value, rules, currentPassword) => {
     isValid = value.length >= rules.minLength && isValid;
     if (!isValid) {
       errorMessage =
-        "Required field length should be greater than " + rules.minLength;
+        "Required field length should be greater than or equal to " + rules.minLength;
     }
   }
 
