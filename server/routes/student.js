e2abbfae3f7ebe9a43dada99fe476a7d6e46be51
mitalirs <mitalirs1701@gmail.com>
2021-05-05 19:42:21 +0530

    added a delete student route
    This enables student to delete hi/her profile. Once the profile is deleted the corresponding internship/job entries get updated (in case they were bookmarked by the student)

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin, update, logout, logoutAll , signupConfirm } = require("../controller/student.auth");
+const { signup, signin, update, logout, logoutAll , signupConfirm, deleteStudent } = require("../controller/student.auth");
 const auth_student = require('../middleware/auth_student');
 const {searchFilterInternships, bookmarkInternship, getBookmarkedInternships} = require("../controller/internships")
 const {searchFilterJobs, bookmarkJob, getBookmarkedJobs} = require("../controller/jobs")
@@ -17,9 +17,10 @@ router.get('/searchFilterJobs', auth_student, searchFilterJobs);
 router.get('/searchFilterFreshersJobs', auth_student, searchFilterFreshersJobs);
 router.get('/confirm/:confirmationCode' , signupConfirm );
 router.post('/bookmarkInternship/:id', auth_student, bookmarkInternship)
-router.post('/bookmarkJob', auth_student, bookmarkJob)
-router.post('/bookmarkFresherJob', auth_student, bookmarkFresherJob)
+router.post('/bookmarkJob/:id', auth_student, bookmarkJob)
+router.post('/bookmarkFresherJob/:id', auth_student, bookmarkFresherJob)
 router.post('/getBookmarkedInternships', auth_student, getBookmarkedInternships)
 router.post('/getBookmarkedJobs', auth_student, getBookmarkedJobs)
 router.post('/getBookmarkedFresherJobs', auth_student, getBookmarkedFresherJobs)
+router.delete('/deleteStudent', auth_student, deleteStudent)
 module.exports = router
