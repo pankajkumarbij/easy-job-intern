955c93019af438316e85ae7ad786bc7accff4f64
Mitali Sonawane <61057219+mitalirs@users.noreply.github.com>
2021-05-31 01:29:36 +0530

    Update student.js

--- server/models/student.js
+++ server/models/student.js
@@ -85,18 +85,18 @@ const studentSchema = new Schema({
     type: String,
     required: true,
   },
-  skills: [{
-    type: String,
+  skills: {
+    [type: String],
     required: true,
-  }],
+  },
   currentRole: {
     type: String,
     required: true,
   },
-  openToRoles: [{
-    type: String,
+  openToRoles: {
+    [type: String],
     required: true,
-  }],
+  },
   workExperience: {
     type: String,
     required: true,
