a64791a29385cdf92dbfdde9971d119f8d4b6192
Ekjot Kaur <ekjotkaurk13@gmail.com>
2021-05-31 23:34:29 +0530

    Education Section

--- src/components/student-profile/StudentProfile/components/Education.js
+++ src/components/student-profile/StudentProfile/components/Education.js
@@ -4,41 +4,42 @@ import { useState } from "react";
 
 import { Col, Row } from "react-bootstrap";
 
-const General = ({ general }) => {
+const Education = ({ Education }) => {
   return (
     <React.Fragment>
       <h2 className="subHeading">Education</h2>
-      <Row className="justify-content-between px-5 py-3">
+      {Education && Education.map(edu => <Row className="justify-content-between px-5 py-3">
+        <Col className="col-12 col-md-12 py-3 ">
+          <div className="div-1">School </div>
+          <div className="div-2">{edu.School}</div>
+        </Col>
+        <Col className="col-12 col-md-12 py-3 ">
+          <div className="div-1">Degree: </div>
+          <div className="div-2">{edu.Degree}</div>
+        </Col>
+        <Col className="col-12 col-md-12 py-3 ">
+          <div className="div-1">FieldOfStudy: </div>
+          <div className="div-2">{edu.FieldOfStudy}</div>
+        </Col>
         <Col className="col-12 col-md-5 py-3 ">
-          <div className="div-1">First Name: </div>
-          <div className="div-2">{general.FirstName}</div>
+          <div className="div-1">StartDate </div>
+          <div className="div-2">{edu.StartDate}</div>
         </Col>
         <Col className="col-12 col-md-5 py-3 ">
-          <div className="div-1">Last Name: </div>
-          <div className="div-2">{general.LastName}</div>
+          <div className="div-1">EndDate: </div>
+          <div className="div-2">{edu.EndDate}</div>
         </Col>
         <Col className="col-12 col-md-12 py-3 ">
-          <div className="div-1">Address: </div>
-          <div className="div-2">{general.Address}</div>
+          <div className="div-1">Grade: </div>
+          <div className="div-2">{edu.Grade}</div>
         </Col>
         <Col className="col-12 col-md-12 py-3 ">
-          <div className="div-1">Github Link: </div>
-          <div className="div-2">{general.GithubLink}</div>
+          <div className="div-1">Description: </div>
+          <div className="div-2">{edu.Description}</div>
         </Col>
-        <Col className="col-12 col-md-12 py-3 ">
-          <div className="div-1">LinkedInLink: </div>
-          <div className="div-2">{general.LinkedInLink}</div>
-        </Col>
-        <div className="div-1 pt-3 pb-2 px-3">Other Profile Link: </div>
-        {general.OtherProfileLink &&
-          general.OtherProfileLink.map((link) => (
-            <Col className="col-12 col-md-12 py-3 ">
-              <div className="div-2">{link && link.ProfileLink}</div>
-            </Col>
-          ))}
-      </Row>
+      </Row>)}
     </React.Fragment>
   );
 };
 
-export default General;
+export default Education;
